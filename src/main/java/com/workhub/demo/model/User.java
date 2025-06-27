7317a3d4372cbdb693b6238fc37678e6385f3830
jaitrasaketh <jaisaketh345@gmail.com>
2025-06-27 22:26:29 +0530

    Add User entity with UUID and basic fields

---
+++ src/main/java/com/workhub/demo/model/User.java
@@ -0,0 +1,38 @@
+package com.workhub.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.*;
+
+@Entity
+@Table(name = "users") // optional if table is named differently
+@Data                   // Generates getters/setters/toString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // UUID auto-gen (Hibernate handles this)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String name;
+
+    @Column(name = "profile_pic_url")
+    private String profilePicUrl;
+
+    // A user can own multiple boards
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
+    private List<Board> boards = new ArrayList<>();
+
+    // A user can collaborate on many boards
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<BoardCollaborator> collaborations = new ArrayList<>();
+
+    // A user can be assigned many tasks
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Assignment> assignments = new ArrayList<>();
+}
