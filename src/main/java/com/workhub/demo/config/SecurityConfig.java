9bef1fd451b3bcd0ca605ae85cdcb9652c20378f
jaitrasaketh <jaisaketh345@gmail.com>
2025-06-30 00:48:05 +0530

    interim security bypass code to test APIs

---
+++ src/main/java/com/workhub/demo/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.workhub.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(org.springframework.security.config.annotation.web.builders.HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .sessionManagement(sess -> sess.sessionCreationPolicy(STATELESS))
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
